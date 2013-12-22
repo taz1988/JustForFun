3ab39085bcbaebfde036dc2bc6d72d657620d58d
taz1988 <taz19880922@gmail.com>
2013-12-22 20:33:11 +0100

    create the base code, which will draw the 5 side of the cube

--- RubicCubeSolver/js/cubeui.js
+++ RubicCubeSolver/js/cubeui.js
@@ -2,7 +2,7 @@
 * This class show the cube on the ui.
 * @author Zoltán Kornél Török, taz19880922@gmail.com
 **/
-function cubeUIClass() { 
+function cubeUIClass(config) { 
     
     this.RED = 0;
     this.YELLOW = 1;
@@ -10,33 +10,63 @@ function cubeUIClass() {
     this.WHITE = 3;
     this.BROWN = 4;
     this.BLUE = 5;
+    
+    this.addNewSlideDiv = function(id, size) {
+         var slide = $('<div id="' + id + '" style="width : ' + size + 'px; height : ' + size + 'px;"></div>');
+         slide.appendTo(this.parent);
+         return slide;
+    }
 
-    this.showSide = function (parentElementId, sideValues) {
+    this.init = function (config) {
+        if (config != null && typeof config !== "undefined" ) {
+            this.parent = $("#" + config.parent);
+            this.top = this.addNewSlideDiv("top", config.size);  
+            this.left = this.addNewSlideDiv("left", config.size);
+            this.center = this.addNewSlideDiv("center", config.size);
+            this.right = this.addNewSlideDiv("right", config.size);
+            this.bottom = this.addNewSlideDiv("bottom", config.size);
+            this.size = config.size;
+        }
+    }
+    this.init(config);
+
+    this.showCube = function (cubeSlides) {
+       var calculatedSize = this.size / cubeSlides[0].length;
+       this.showSide(this.top, cubeSlides[0], calculatedSize);
+       this.showSide(this.left, cubeSlides[1], calculatedSize);
+       this.showSide(this.center, cubeSlides[2], calculatedSize);
+       this.showSide(this.right, cubeSlides[3], calculatedSize);
+       this.showSide(this.bottom, cubeSlides[4], calculatedSize);
+    }
+    
+    this.showSide = function (parentElementId, sideValues, size) {
         var i, j;
         for (i = 0; i < sideValues.length; i++) {
            for (j = 0; j < sideValues[i].length; j++) {
                 var newSidePart = null;
                 switch (sideValues[i][j]) {
                 case this.RED:
-                    newSidePart = "<div class='btn btn-danger'></div>";
+                    newSidePart = $("<div class='btn btn-danger'></div>");
                     break;
                 case this.YELLOW:
-                    newSidePart = "<div class='btn btn-warning'></div>";
+                    newSidePart = $("<div class='btn btn-warning'></div>");
                     break;
                 case this.GREEN:
-                    newSidePart = "<div class='btn btn-success'></div>";
+                    newSidePart = $("<div class='btn btn-success'></div>");
                     break;
                 case this.WHITE:
-                    newSidePart = "<div class='btn btn-default'></div>";
+                    newSidePart = $("<div class='btn btn-default'></div>");
                     break;
                 case this.BROWN:
-                    newSidePart = "<div class='btn btn-default brown'></div>";
+                    newSidePart = $("<div class='btn btn-default brown'></div>");
                     break;
                 case this.BLUE:
-                    newSidePart = "<div class='btn btn-primary'></div>";
+                    newSidePart = $("<div class='btn btn-primary'></div>");
                     break;
                 }
-                $(parentElementId).append(newSidePart);            
+                newSidePart.appendTo(parentElementId);
+                newSidePart.css("width", size + "px");
+                newSidePart.css("height", size + "px");
             } 
         }
     }   
--- RubicCubeSolver/unittests/cubeuitests.js
+++ RubicCubeSolver/unittests/cubeuitests.js
@@ -1,88 +1,114 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
-});
 
 test("CreateCubeOneSide", function() {
-	$("html").append("<div id='test'></div>");
-	var WIDTH = 100;
-	var HEIGHT = 100;
-	$("#test").width(WIDTH);
-    $("#test").height(HEIGHT);
+    $("html").append("<div id='test2'></div>");
+    var WIDTH = 100;
+    var HEIGHT = 100;
+    $("#test2").width(WIDTH);
+    $("#test2").height(HEIGHT);
     var cubeUI = new cubeUIClass();
-	var side = [[],[],[]];
-	side[0][0] = cubeUI.RED;
-	side[0][1] = cubeUI.WHITE;
-	side[0][2] = cubeUI.YELLOW;
-	side[1][0] = cubeUI.BLUE;
-	side[1][1] = cubeUI.GREEN;
-	side[1][2] = cubeUI.BROWN;
-	side[2][0] = cubeUI.YELLOW;
-	side[2][1] = cubeUI.RED;
-	side[2][2] = cubeUI.YELLOW;	
-	cubeUI.showSide("#test", side);
-	console.log($("#test").children());
-	equal($("#test").children().length, 9, "The parent div have to 9 child element exactly!");
-	equal($("#test").outerWidth(), WIDTH, "The Width is not good");
-	$("#test").remove();
+    var side = [[],[],[]];
+    side[0][0] = cubeUI.RED;
+    side[0][1] = cubeUI.WHITE;
+    side[0][2] = cubeUI.YELLOW;
+    side[1][0] = cubeUI.BLUE;
+    side[1][1] = cubeUI.GREEN;
+    side[1][2] = cubeUI.BROWN;
+    side[2][0] = cubeUI.YELLOW;
+    side[2][1] = cubeUI.RED;
+    side[2][2] = cubeUI.YELLOW; 
+    cubeUI.showSide($("#test2"), side);
+    console.log($("#test2").children());
+    equal($("#test2").children().length, 9, "The parent div have to 9 child element exactly!");
+    equal($("#test2").outerWidth(), WIDTH, "The Width is not good");
+    $("#test2").remove();
 });
 
 test("testCreateCubeOneSideUseDifferentClassNames", function() {
-	$("html").append("<div id='test'></div>");
-	var WIDTH = 600;
-	var HEIGHT = 600;
-	$("#test").width(WIDTH);
+    $("html").append("<div id='test'></div>");
+    var WIDTH = 600;
+    var HEIGHT = 600;
+    $("#test").width(WIDTH);
         $("#test").height(HEIGHT);
         var cubeUI = new cubeUIClass();
-	var side = [[],[],[]];
-	side[0][0] = cubeUI.RED;
-	side[0][1] = cubeUI.WHITE;
-	side[0][2] = cubeUI.YELLOW;
-	side[1][0] = cubeUI.BLUE;
-	side[1][1] = cubeUI.GREEN;
-	side[1][2] = cubeUI.BROWN;
-	side[2][0] = cubeUI.YELLOW;
-	side[2][1] = cubeUI.RED;
-	side[2][2] = cubeUI.YELLOW;	
-	redCount = 2;
-	whiteCount = 1;
-	yellowCount = 3;
-	blueCount = 1;
-	brownCount = 1;
-	greenCount = 1;
-	cubeUI.showSide("#test", side);
-	$("#test").children().each(function() {
-		if($(this).hasClass("btn-danger")) {
-			redCount--;
-		}
-		
-		if($(this).hasClass("btn-warning")) {
-			yellowCount--;
-		}
+    var side = [[],[],[]];
+    side[0][0] = cubeUI.RED;
+    side[0][1] = cubeUI.WHITE;
+    side[0][2] = cubeUI.YELLOW;
+    side[1][0] = cubeUI.BLUE;
+    side[1][1] = cubeUI.GREEN;
+    side[1][2] = cubeUI.BROWN;
+    side[2][0] = cubeUI.YELLOW;
+    side[2][1] = cubeUI.RED;
+    side[2][2] = cubeUI.YELLOW; 
+    redCount = 2;
+    whiteCount = 1;
+    yellowCount = 3;
+    blueCount = 1;
+    brownCount = 1;
+    greenCount = 1;
+    cubeUI.showSide($("#test"), side);
+    $("#test").children().each(function() {
+        if($(this).hasClass("btn-danger")) {
+            redCount--;
+        }
+        
+        if($(this).hasClass("btn-warning")) {
+            yellowCount--;
+        }
 
-		if($(this).hasClass("btn-success")) {
-			greenCount--;
-		}
+        if($(this).hasClass("btn-success")) {
+            greenCount--;
+        }
 
-		if($(this).hasClass("btn-default")) {
-			whiteCount--;
-		}
+        if($(this).hasClass("btn-default")) {
+            whiteCount--;
+        }
 
-		if($(this).hasClass("brown")) {
-			brownCount--;
-			whiteCount++;
-		}
+        if($(this).hasClass("brown")) {
+            brownCount--;
+            whiteCount++;
+        }
 
-		if($(this).hasClass("btn-primary")) {
-			blueCount--;
-		}
+        if($(this).hasClass("btn-primary")) {
+            blueCount--;
+        }
 
-	});
-	equal($("#test").children().length, 9, "The parent div have to 9 child element exactly!");
-	equal(redCount, 0, "The class count should be 0!");
-	equal(whiteCount, 0, "The class count should be 0!");
-	equal(yellowCount, 0, "The class count should be 0!");
-	equal(blueCount, 0, "The class count should be 0!");
-	equal(brownCount, 0, "The class count should be 0!");
-	equal(greenCount, 0, "The class count should be 0!");	
-   	$("#test").remove();
+    });
+    equal($("#test").children().length, 9, "The parent div have to 9 child element exactly!");
+    equal(redCount, 0, "The class count should be 0!");
+    equal(whiteCount, 0, "The class count should be 0!");
+    equal(yellowCount, 0, "The class count should be 0!");
+    equal(blueCount, 0, "The class count should be 0!");
+    equal(brownCount, 0, "The class count should be 0!");
+    equal(greenCount, 0, "The class count should be 0!");   
+    $("#test").remove();
 });
+
+test("draw5Slide", function() {
+    $("html").append("<div id='test'></div>");
+    var WIDTH = 100;
+    var HEIGHT = 100;
+    $("#test").width(WIDTH);
+    $("#test").height(HEIGHT);
+    var cubeUI = new cubeUIClass({ parent : "test", size : 200});
+    var side = [[],[],[]];
+    side[0][0] = cubeUI.RED;
+    side[0][1] = cubeUI.WHITE;
+    side[0][2] = cubeUI.YELLOW;
+    side[1][0] = cubeUI.BLUE;
+    side[1][1] = cubeUI.GREEN;
+    side[1][2] = cubeUI.BROWN;
+    side[2][0] = cubeUI.YELLOW;
+    side[2][1] = cubeUI.RED;
+    side[2][2] = cubeUI.YELLOW;
+    var cube = [side, side, side, side, side]; 
+    cubeUI.showCube(cube);
+    console.log($("#test").children());
+    equal($("#test").children().length, 5, "The parent div have to 5 child element exactly!");
+    $("#test").children().each(function () {
+        equal($(this).children().length, 9, "It should be 9 child!");
+    });
+    $("#test").remove();
+});
+
+
